e059c52168c0affa159d853426d2e10f7b608457
ivandisi <ivan.disir@gmail.com>
2026-01-01 13:30:50 +0100

    rework year view

--- android app/app/src/main/java/it/id/pistacchio/view/YearView.kt
+++ android app/app/src/main/java/it/id/pistacchio/view/YearView.kt
@@ -6,19 +6,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import it.id.pistacchio.view.generic.Header
 import it.id.pistacchio.view.generic.YearRowChart
+import it.id.pistacchio.viewmodel.YearViewModel
+import java.util.Locale
 
 @Composable
-fun YearView() {
+fun YearView(yearViewModel: YearViewModel = viewModel() ) {
     Column {
         Header()
 
         Text(
-            text = "This year I'm doing ...",
+            text = "This year I'm doing a total of ${yearViewModel.totalLength.value.let {  String.format(Locale.getDefault(), "%.2f", it) }}km ...",
             modifier = Modifier
                 .padding(8.dp)
         )
+
         YearRowChart(
             modifier = Modifier
                 .padding(8.dp)
--- android app/app/src/main/java/it/id/pistacchio/viewmodel/YearViewModel.kt
+++ android app/app/src/main/java/it/id/pistacchio/viewmodel/YearViewModel.kt
@@ -12,13 +12,17 @@ import it.id.pistacchio.Constants
 import it.id.pistacchio.net.MyApi
 import it.id.pistacchio.net.model.DataByYear
 import it.id.pistacchio.ui.theme.PRIMARY
+import it.id.pistacchio.ui.theme.SECONDARY
 import retrofit2.Response
 import java.time.LocalDate
 
 class YearViewModel : ViewModel() {
 
     private val _dataList = mutableStateOf<List<Bars>>(emptyList())
+    var _totalLength = mutableStateOf<Float>(0.0F)
+
     val dataList: State<List<Bars>> = _dataList
+    var totalLength: State<Float> = _totalLength
 
     init {
         _dataList.value = listOf(
@@ -38,8 +42,11 @@ class YearViewModel : ViewModel() {
                 if (data != null) {
 
                     val mydata = arrayListOf<Bars>()
+                    _totalLength.value = 0.0f
 
                     for ((index, item) in data.withIndex()) {
+                        _totalLength.value += item.length.toFloat() / 100000
+
                         mydata.add(
                             Bars(
                                 Constants.Support.MONTHS.get(item.month.toInt()-1),
@@ -47,9 +54,15 @@ class YearViewModel : ViewModel() {
                                     label = "Trips",
                                     value = item.trips.toDouble(),
                                     color = SolidColor(PRIMARY)
-                                )
+                                ),
+                                Bars.Data(
+                                    label = "Distance in meter",
+                                    value = item.length.toDouble() / 100,
+                                    color = SolidColor(SECONDARY)
                                 ))
+                            )
                         )
+
                     }
                     _dataList.value = mydata
                 }
